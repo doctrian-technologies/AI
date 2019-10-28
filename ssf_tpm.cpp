1b3ae762b8c6ef4c322b19542ea93029f67625f0
S0u1Sp101T <51326285+SoulSploit@users.noreply.github.com>
2019-10-28 19:03:41 +0000

    Sigmoid() static function and the Pulse() method
    This means our entire network runs with values x where 0 < x < 1, or alternatively x: (0,1) (x is between 0 and 1).

--- ssf_tpm.cpp
+++ ssf_tpm.cpp
@@ -2,3 +2,15 @@ private static double Sigmoid(double value)
 {  
       return 1 / (1 + Math.Exp(-value));  
 }  
+
+public void Pulse(TriNeuralLayer layer)  
+{  
+    lock (this)  
+    {  
+        m_output = 0;  
+        foreach (KeyValuePair<TriNeuronSignal, NeuralFactor> item in m_input)  
+            m_output += item.Key.Output * item.Value.Weight;  
+        m_output += m_bias.Weight * BiasWeight;  
+        m_output = Sigmoid(m_output);  
+    }  
+} 
