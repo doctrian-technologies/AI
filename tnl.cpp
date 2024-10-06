7e0caf6dff3260f135eebd52c21a979f719dab25
S0u1Sp101T <51326285+SoulSploit@users.noreply.github.com>
2024-10-06 11:54:41 +0200

    Update tnl.cpp
    Key Enhancements:
    
    Documentation: Added XML comments to clarify the purpose of each method and its parameters.
    Interface Consistency: Used ITriNeuron and ITriNeuralNet to ensure consistency and clarity in type references.
    Code Clarity: Comments were added to explain the steps being taken within each method.
    
    
    Signed-off-by: S0u1Sp101T <51326285+SoulSploit@users.noreply.github.com>

--- tnl.cpp
+++ tnl.cpp
@@ -1,10 +1,25 @@
-public void Pulse(TriNeuralNet net)  
+/// <summary>
+/// Activates each neuron in the layer using the specified neural network context.
+/// </summary>
+/// <param name="net">The neural network instance used for processing.</param>
+public void Pulse(ITriNeuralNet net)  
 {  
-    foreach (TriNeuron n in m_neurons)  
-        n.Pulse(this);  
-}  
-public void ApplyLearning(TriNeuralNet net)  
+    // Activate each neuron in the layer
+    foreach (ITriNeuron neuron in m_neurons)  
+    {  
+        neuron.Pulse(this);  
+    }  
+}
+
+/// <summary>
+/// Applies the learning algorithm to each neuron in the layer based on the neural network context.
+/// </summary>
+/// <param name="net">The neural network instance used for learning.</param>
+public void ApplyLearning(ITriNeuralNet net)  
 {  
-    foreach (TriNeuron n in m_neurons)  
-        n.ApplyLearning(this);  
-}  
+    // Update weights for each neuron in the layer
+    foreach (ITriNeuron neuron in m_neurons)  
+    {  
+        neuron.ApplyLearning(this);  
+    }  
+}
